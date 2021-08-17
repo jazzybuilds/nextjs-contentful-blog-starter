817fdd473fd40bd7d979550498a0eb8e3619fdbe
jazzybuilds <Jason@Users-MacBook-Air.local>
2021-08-17 14:11:24 +0800

    testing netlify.toml

--- components/Post/Tags/index.js
+++ components/Post/Tags/index.js
@@ -1,4 +1,5 @@
 import TagsStyles from "@styles/Tags.module.css";
+import { Config } from "@utils/Config";
 
 export default function Tags(props) {
   const { tags } = props;
@@ -6,10 +7,16 @@ export default function Tags(props) {
   return (
     <ul className={TagsStyles.tags}>
       {tags.map((tag) => (
+        <a href={`/blog/tag/${tag.id}`}>
         <li className={TagsStyles.tags__tag} key={tag.id}>
           {tag.id}
         </li>
+        </a>
       ))}
     </ul>
   );
 }
+
+
+
+
